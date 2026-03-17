cd2ebe21bb8d37e9fec5862cff3ec923eae5febc
Francis <franciskarani15@gmail.com>
2026-03-17 22:56:46 +0300

    creates the category section with divs

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,11 @@
 import HeroSection from '../components/HeroSection'
+import Categories from '../components/Categories'
 
 export default function LandingPage() {
   return (
     <div>
       <HeroSection />
+      <Categories />
     </div>
   )
 }
